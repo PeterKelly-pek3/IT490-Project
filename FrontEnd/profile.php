2b1380aedbc252583b9b6fe1dd1d2dd4ed83c6d9
PeterKelly-pek3 <98290955+PeterKelly-pek3@users.noreply.github.com>
2022-03-19 15:23:45 -0400

    Midterm - Final Final Update
    Tested and everything works

---
+++ FrontEnd/profile.php
@@ -0,0 +1,54 @@
+<?php
+
+session_start();
+
+//Check login and redirect
+if (!$_SESSION["logged"])
+{
+	header("Location: /loginRegister.html");
+}
+
+
+include('webClient.php');
+include('webFunctions.php');
+//include('errors.php');
+
+
+//Error Reporting
+error_reporting(E_ALL);
+ini_set('display_errors', 'Off');
+ini_set('log_errors', 'On');
+ini_set('error_log', '/home/testserver/git/rabbitmqphp_example/FrontEnd/Logs/errLog.txt');
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>League of Legends Fantasy League Profile</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	<body>
+		<h2>Create Your User Profile</h2>
+		<form method="post" action="profile.php">
+			<h3>User Profile</h3>
+			<div class="input-group">
+
+  	        		<button type="submit" class="btn4" name="test_api">Do NOT Click</button>
+  			</div>
+  			<p><a href="logout.php?logout='1'" style="color: red;">Logout</a></p>
+		</form>
+
+		<form method="get" action="Esports_Chat/index.html">
+                        <h3>Global & Team Chat</h3>
+                        <div class="input-group">
+
+                                <button type="submit" class="btn4" name="chat">Chat</button>
+                        </div>
+                        <p><a href="logout.php?logout='1'" style="color: red;">Logout>
+                </form>
+	<script type="text/javascript" src="webScripts.js">
+	</script>
+	</body>
+</html>
