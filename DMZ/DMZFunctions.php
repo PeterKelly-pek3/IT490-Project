fa9d7928dff41788a686ad1d8c0dc65d03ce085b
DanConstancia <33687822+DanConstancia@users.noreply.github.com>
2022-05-04 14:12:52 -0400

    Update DMZFunctions.php

--- DMZ/DMZFunctions.php
+++ DMZ/DMZFunctions.php
@@ -48,6 +48,42 @@ else
 }
 }
 
+//Test API Function
+function GetOdds($eventID)
+{
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+    CURLOPT_URL => "https://api.b365api.com/v2/event/odds?token=115215-MDpRLi6nUUlglr&event_id=".$eventID,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 30,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "GET",
+
+]);
+	
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err)
+{
+    	echo "cURL Error #:" . $err;
+}
+else
+{
+    	$data = json_decode($response, true);
+	$sendData = $data;
+	return $sendData;
+
+}
+}
+
 // Get historical data function
 function getHistorical()
 {
