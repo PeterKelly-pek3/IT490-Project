b1d4b3e96b3167b8f4703d0db8faba66ebd4064d
PeterKelly-pek3 <98290955+PeterKelly-pek3@users.noreply.github.com>
2022-05-05 19:03:43 -0400

    Update DBFunctions.php

--- DB/DBFunctions.php
+++ DB/DBFunctions.php
@@ -187,7 +187,7 @@ function getOdds()
 			print_r($integerIDs);
 			//echo $event;
 			
-			$query = "INSERT INTO LeagueData (Home_Odds, Away_Odds) VALUES ('$Home_Odds','$Away_Odds') WHERE eventID=$integerIDs ";
+			$query = "INSERT INTO LeagueData (Home_Odds, Away_Odds) VALUES ('$Home_Odds','$Away_Odds') WHERE eventID='$integerIDs' ";
 			$result = $connection->query($query);
 		}
 		
