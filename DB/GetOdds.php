f257fd4c12005e70c70031fa539d2c29d698793d
DanConstancia <33687822+DanConstancia@users.noreply.github.com>
2022-05-05 14:43:39 -0400

    Add files via upload

---
+++ DB/GetOdds.php
@@ -0,0 +1,16 @@
+<?php
+#!/usr/bin/php
+<?php
+
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+require_once('DBFunctions.php');
+
+echo "Retrieving Odds API Data..\n";
+
+getOdds();
+
+echo "Finished Retrieving Odds API Data.\n";
+
+?>
